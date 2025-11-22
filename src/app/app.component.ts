53d66783254dbf81403dfdffda85a91ee16f9c46
SkyNaoto <32412827+SkyNaoto@users.noreply.github.com>
2025-11-22 10:29:31 +0900

    今月、先月、過去３０日ボタン追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,43 @@ export class AppComponent {
 
   constructor(private searchService: SearchService) {}
 
+  setThisMonth() {
+    const now = new Date();
+    const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
+    this.params.startDate = this.formatDate(firstDay);
+    this.params.endDate = this.formatDate(now);
+    // this.doSearch();
+  }
+  setLastMonth() {
+    const now = new Date();
+    // 前月の初日を取得
+    const firstDayLastMonth = new Date(now.getFullYear(), now.getMonth()-1, 1);
+    // 前月の最終日を取得 0は当月の0日目、つまり前月の最終日を指す
+    const lastDayLastMonth = new Date(now.getFullYear(), now.getMonth(), 0);
+    this.params.startDate = this.formatDate(firstDayLastMonth);
+    this.params.endDate = this.formatDate(lastDayLastMonth);
+    // this.doSearch();
+  }
+  setLast30Days(){
+    const now = new Date();
+    const past = new Date();
+    past.setDate(now.getDate() - 30);
+    this.params.startDate = this.formatDate(past);
+    this.params.endDate = this.formatDate(now);
+    // this.doSearch();
+  }
+
+  formatDate(date: Date): string {
+    // getFullYear()は西暦年を取得する
+    const y = date.getFullYear();
+    // slice(-2)は、文字列の末尾から2文字を取得する
+    // getMonth()は0から始まるため、1を足してから2桁にフォーマットする
+    const m = ('0' + (date.getMonth() + 1)).slice(-2);
+    // getDate()は日付を取得する
+    const d = ('0' + date.getDate()).slice(-2);
+    return `${y}-${m}-${d}`;
+  }
+
   doSearch() {
     this.loading = true;
     this.searchService.search(this.params).subscribe({
